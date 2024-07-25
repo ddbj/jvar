70f5b3b9a6b7f9751f6c16700a42e7f9b6cd34ca
Kodama Yuichi <komstat@users.noreply.github.com>
2024-07-25 23:33:58 +0900

    Add files via upload

--- script/vcf-id-uniq.rb
+++ script/vcf-id-uniq.rb
@@ -32,16 +32,33 @@ vcf_file_a.each{|vcf_file|
 	vcf_f.each_line{|line|
 	
 		if line[0] == "#"
-			vcf_uniq_f.puts line
+			if line =~ /^##INFO=<ID=ExcHet,Number=A/
+				vcf_uniq_f.puts line
+				vcf_uniq_f.puts '##INFO=<ID=CMT=1,Type=String,Description="Comment">'
+			else
+				vcf_uniq_f.puts line
+			end			
 		else
 			vcf_line_a = line.split("\t")
+			
+			chr = vcf_line_a[0]
+			pos = vcf_line_a[1]
 			id = vcf_line_a[2]
+			ref = vcf_line_a[3]
+			alt = vcf_line_a[4]
+			info = vcf_line_a[7]
 
+			if id.include? "_"
+				vcf_line_a[7] = "#{vcf_line_a[7]};CMT=\"Original ID - #{chr}:#{pos}:#{ref}:#{alt}\""
+			end
+			
 			if id_h.has_key?(:"#{id}")
 				vcf_uniq_log_f.puts "#{id}"
 			else
-				vcf_uniq_f.puts line
+				vcf_uniq_f.puts vcf_line_a.join("\t")
 			end
+
+			if 
 			
 			# ID uniq
 			id_h.store(:"#{id}", 1)
